02782e285c4aefcb4e8e57c4a243877ae93721ad
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-08-01 12:51:55 -0400

    API pulling correctly and displaying in the DOM

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,23 +3,24 @@
     <div>
       <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
         <template slot="header">What's Trending</template>
-        <p id = "message">Search to find out what's trending</p>
+        <p id="message">Search to find out what's trending</p>
         <template>
           <form>
             <input type="text" v-model="search.userInput" />
             <br />
             <button type="submit" class="btn btn-primary" @click.prevent="handleSubmit">Trending</button>
             <!-- <button type="clear" class="btn btn-primary clear" @click="clear">Clear</button> -->
-            <div id = "searchResult">
-           
-             
-          </div>
           </form>
-          
         </template>
-
-        <hr class="my-4" />
       </b-jumbotron>
+      <div id="searchResult" v-for="(result,index) in search.results" v-bind:key="index">
+        <h2>{{search.results[index].title}}</h2>
+        <p>{{search.results[index].snippet}}</p>
+        <a :href="search.results[index].link">
+          {{search.results[index].link}}
+        </a>
+       
+      </div>
     </div>
   </div>
 </template>
@@ -28,13 +29,13 @@
 // @ is an alias to /src
 import axios from "axios";
 export default {
+  // el: '#search',
   data() {
     return {
       search: {
         userInput: "",
-        googleKey:"AIzaSyBRlj_omJsZWTgEIXq9yLePCL_HNfIfdkk",
-        googleSearch : "https://www.googleapis.com/customsearch/v1?key=" + this.search.googleKey + "&cx=015376139325119918930:rwhxpl8byui&q=" + this.search.searchInput,
-        replace : this.search.googleSearch.replace(" ", "%20")
+        googleKey: "AIzaSyBRlj_omJsZWTgEIXq9yLePCL_HNfIfdkk",
+        results: []
       }
     };
   },
@@ -42,15 +43,24 @@ export default {
     handleSubmit() {
       axios
         .get(
-        //  "https://www.googleapis.com/customsearch/v1?key="+ this.googleKey+"&cx=015376139325119918930:rwhxpl8byui&q="+this.searchInput,
-        this.search.replace
+          "https://www.googleapis.com/customsearch/v1?key=" +
+            this.search.googleKey +
+            "&cx=015376139325119918930:rwhxpl8byui&q=" +
+            this.search.userInput
         )
         .then(response => {
-          console.log(response);
-       
+          console.log(this.search.userInput);
+          console.log(response.data);
+          this.search.results = response.data.items;
+          console.log(this.search.results);
         });
-    },
-
+    }
   }
 };
 </script>
+<style scoped>
+#searchResult{
+  text-align: left;
+}
+</style>
+
